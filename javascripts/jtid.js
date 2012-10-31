81c089fdd6d0b32708186bafee1caa5c2416cff8
André Ruffert <a.ruffert@gmail.com>
2012-10-31 16:25:15 +0100

    renamed the exec function

--- javascripts/jtid.js
+++ javascripts/jtid.js
@@ -1,8 +1,8 @@
-function getJiraTickedId() {
+function exec() {
     var keyVal      = document.getElementById('key-val').text,
         summaryVal  = document.getElementById('summary-val').getElementsByTagName('a')[0].text
     ;
     prompt('JIRA Ticket-ID', keyVal + ';' + summaryVal);
 }
 
-getJiraTickedId();
+exec();
