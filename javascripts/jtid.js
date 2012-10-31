0df84f4bc00de7db5b68fdec86e2a92e1e705ac7
André Ruffert <a.ruffert@gmail.com>
2012-10-31 15:51:48 +0100

    added the getJiraTickedId functionality

---
+++ javascripts/jtid.js
@@ -0,0 +1,8 @@
+function getJiraTickedId() {
+    var keyVal      = document.getElementById('key-val').text,
+        summaryVal  = document.getElementById('summary-val').getElementsByTagName('a')[0].text
+    ;
+    prompt('JIRA Ticket-ID', keyVal + ';' + summaryVal);
+}
+
+getJiraTickedId();
